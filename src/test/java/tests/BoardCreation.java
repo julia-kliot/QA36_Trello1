1d01910c1bbf8858f5d9e1adb126b9e762321a48
julia-kliot <juliakliot.jk@gmail.com>
2023-01-16 20:44:36 +0300

    boardCreation+model

---
+++ src/test/java/tests/BoardCreation.java
@@ -0,0 +1,44 @@
+package tests;
+
+import model.Board;
+import model.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.jws.soap.SOAPBinding;
+
+public class BoardCreation extends TestBase{
+
+    @BeforeMethod
+    public void  preConditions(){
+       User user = new User().withEmail("juliakliot.jk@gmail.com").withPassword("misha240613");
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm(user);
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+    }
+
+    @Test
+
+    public void boardCreation1(){
+        Board board = new Board().withTitle("qa36");
+
+        app.getBoard().initBoardCreation();
+        app.getBoard().fillInBoardCreationForm(board);
+        app.getBoard().scrollDownTheForm();
+        app.getBoard().pause(2000);
+        app.getBoard().submitBoardCreation();
+        app.getBoard().pause(2000);
+        app.getBoard().isCreated();
+
+        Assert.assertTrue(app.getBoard().isCreated());
+
+        app.getBoard().pause(2000);
+        app.getBoard().returnToHomePage();
+
+
+    }
+}
--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,8 +14,8 @@ import java.time.Duration;
 public class Login extends TestBase {
 
     @BeforeMethod
-    public void preConditions(){
-        if(app.getUser().isLogged()){
+    public void preConditions() {
+        if (app.getUser().isLogged()) {
             app.getUser().logOut();
         }
     }
@@ -22,33 +23,28 @@ public class Login extends TestBase {
 
     @Test
     public void login1() {
+        User user = new User().withEmail("juliakliot.jk@gmail.com").withPassword("misha240613");
         app.getUser().initLogin();
         app.getUser().pause(2000);
-        app.getUser().fillInLoginForm("juliakliot.jk@gmail.com","misha240613");
+        app.getUser().fillInLoginForm(user);
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+    }
+
+    @Test
+    public void login2() {
+
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm("juliakliot.jk@gmail.com", "misha240613");
         app.getUser().submitLogin();
         app.getUser().pause(2000);
 
         Assert.assertTrue(app.getUser().isLogged());
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
 }
